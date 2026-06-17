b3d5007cf7eaea2fde29e59ecaac2461247bacb3
tokunaganatsuki <tokunaganatsuki0831@gmail.com>
2026-06-17 23:40:45 +0900

    User.nicknameのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, length: { maximum: 20 }
+  validates :nickname, length: { maximum: 20 }, presence: true
 end
